c767e8cdd417b9b6817aae586346517b781ce3d9
oLOWONLA <olowonlafaruq@gmail.com>
2024-02-22 02:44:29 +0100

    change

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
 
   return (
     <>
-    <main className="w-full h-screen overflow-x-hidden scroll-smooth bg-hero" style={{background:`url("/src/assets/chaseimg/chasecakebg.png)`}} >
+    <main className="w-full h-screen overflow-x-hidden scroll-smooth bg-hero" >
 <Navbar/>
 <Home/>
 <About/>
